6b5c16ec667087cfe996be0f18a7435222510712
mohdshahzil <theshahzil@gmail.com>
2024-04-17 19:16:06 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.Text}>22MIS1161</Text>
+      <Button title="Mohammad Shahzil "> </Button>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#282828",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Text: {
+    color: "white",
+    fontSize: 20,
   },
 });
